4a2d54ed9fac6264cb611d09d7db98c6ab51950d
root <740028467@qq.com>
2021-11-26 16:08:13 +0800

    fix bug

--- ansible/roles/mongodb-common/files/opt/app/current/bin/node/05_nodectl_third.sh
+++ ansible/roles/mongodb-common/files/opt/app/current/bin/node/05_nodectl_third.sh
@@ -9,14 +9,13 @@ reloadZabbix() {
 }
 
 reloadNodeExporter() {
+  systemctl daemon-reload
   log "start to reload node_exporter"
   if [ $NODE_EXPORTER_ENABLED = "yes" ]; then
-    # 需要先杀掉已经存在的nodeExporter进程
-#     kill `netstat -nultp | grep node_exporter | awk '{print $7}' | awk -F "/" '{print $1}'`   || :
-    nohup /opt/node_exporter/current/node_exporter-1.2.2.linux-amd64/node_exporter  --web.listen-address=":$NODE_EXPORTER_PORT" &
+    systemctl restart node_exporter.service || :
     log "node_exporter restarted"
   else
-    kill `netstat -nultp | grep $NODE_EXPORTER_PORT | awk '{print $7}' | awk -F "/" '{print $1}'`   || :
+    systemctl stop node_exporter.service || :
     log "node_exporter stopped"
   fi
 
@@ -24,33 +23,35 @@ reloadNodeExporter() {
 
 reloadCaddy() {
   if [ $CADDY_ENABLED = "yes" ]; then
-    caddy start --config $CONF_CADDY_INFO_FILE
+    systemctl restart caddy.service || :
     log "caddy restarted"
   else
-    caddy stop || :
+    systemctl stop caddy.service || :
     log "caddy stopped"
   fi
 }
 
 reloadMongoDBExporter () {
+  systemctl daemon-reload
   log "start to reload mongodb_exporter"
   if [ $MONGODB_EXPORTER_ENABLED = "yes" ]; then
-    kill `netstat -nultp | grep mongodb_exporter | awk '{print $7}' | awk -F "/" '{print $1}'`   || :
-    nohup /usr/bin/mongodb_exporter --mongodb.uri=mongodb://$DB_MONITOR_USER:$DB_MONITOR_PWD@localhost:$MY_PORT --web.listen-address=:$MONGODB_EXPORTER_PORT &
+    systemctl restart mongodb_exporter.service || :
     log "mongodb_exporter restarted"
   else
-    kill `netstat -nultp | grep $MONGODB_EXPORTER_PORT | awk '{print $7}' | awk -F "/" '{print $1}'`   || :
+    systemctl stop mongodb_exporter.service || :
     log "mongodb_exporter stopped"
   fi
 }
 
 reloadMongoShake() {
   log "start to reload mongoshake"
-  kill `netstat -nultp | grep collector.li | awk '{print $7}' | awk -F "/" '{print $1}'`   || :
+
   if [ $MONGOSHAKE_ENABLED = "yes" ]; then
     touch $MONGOSHAKE_FLAG_FILE
-    nohup /opt/mongo-shake/current/mongo-shake-v2.6.5/collector.linux -conf=$CONF_MONGOSHAKE_FILE &
+    systemctl restart mongoshake.service || :
     log "mongoshake started"
+  else 
+    systemctl stop mongoshake.service || :
   fi
   rm -rf $MONGOSHAKE_FLAG_FILE || : 
 
