98b58b5ff536cda5806089226921125ca88ec412
chenxin <740028467@qq.com>
2022-04-06 11:38:47 +0800

    fix

--- ansible/roles/mongodb-common/files/opt/app/current/bin/node/01_nodectl_lib.sh
+++ ansible/roles/mongodb-common/files/opt/app/current/bin/node/01_nodectl_lib.sh
@@ -8,6 +8,15 @@ IGNORE_HEALTH_CHECK_FLAG_FILE=/data/appctl/data/ignore_health.check.flag
 MONGOSHAKE_CONF=/opt/app/current/conf/mongoshake/mongoshake.conf
 BEING_HEALTH_CHECKED_FLAG=/data/appctl/data/being_health_checked.flag
 BEING_REVIVED_FLAG=/data/appctl/data/being_revived.flag
+MONITOR_DIR=/opt/mongo-shake/metrics
+MONITOR_ITEM_EXECUTOR=/opt/mongo-shake/metrics/executor.metrics
+MONITOR_ITEM_PERSIST=/opt/mongo-shake/metrics/persist.metrics
+MONITOR_ITEM_QUEUE=/opt/mongo-shake/metrics/queue.metrics
+MONITOR_ITEM_REPL=/opt/mongo-shake/metrics/repl.metrics
+MONITOR_ITEM_SENTINEL=/opt/mongo-shake/metrics/sentinel.metrics
+MONITOR_ITEM_SENTINEL_OPTIONS=/opt/mongo-shake/metrics/sentinel_options.metrics
+MONITOR_ITEM_WORKER=/opt/mongo-shake/metrics/worker.metrics
+
 
 start(){
     log ">>>>> start <<<<<"
@@ -26,7 +35,53 @@ reload(){
 
 monitor(){
     log ">>>>> monitor <<<<<"
+    if [ ! -d $MONITOR_DIR ];then
+        mkdir -p $MONITOR_DIR 
+    fi
+    local res
+    local title
+    local value
+    local port=$(getItemFromFile incr_sync.http_port $MONGOSHAKE_CONF)
+    getMonitorItem $MONITOR_ITEM_EXECUTOR "executor"
+    getMonitorItem $MONITOR_ITEM_PERSIST "persist"
+    getMonitorItem $MONITOR_ITEM_QUEUE "queue"
+    getMonitorItem $MONITOR_ITEM_REPL "repl"
+    getMonitorItem $MONITOR_ITEM_SENTINEL "sentinel"
+    getMonitorItem $MONITOR_ITEM_SENTINEL_OPTIONS "sentinel_options"
+    getMonitorItem $MONITOR_ITEM_WORKER "worker"
+
+
+    if [ ! -s $MONITOR_ITEM_EXECUTOR ]; then
+        res=$(outputItem $MONITOR_ITEM_EXECUTOR "executor")
+    fi
+
+
 }
+
+outputItem() {
+    local group
+    local title
+    local value
+    while read line; do
+        group=$(echo $line | cut -d'/' -f1)
+        title=$(echo $line | cut -d'/' -f2)
+        if [ "$group" = "$2" ]; then
+            if [ "$2" = "executor" ]; then
+                
+            else
+                value=$(cat $1 |jq "$title")
+            fi
+        fi
+        res="$res,\"$title\":$value"
+    done < $1
+    echo $res
+}
+
+getMonitorItem(){
+    curl 127.0.0.1:$port/$2 > $1
+}
+
+
 
 healthCheck(){
     log ">>>>> healthCheck begin <<<<<"
